d8b8171b0d8fbcab7105f6c39ed84c60e8c724a3
Andrew Nichols <tandrewnichols@gmail.com>
2014-09-19 16:51:49 -0400

    Make compatible with lodash and underscore on both client and server

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,8 @@ var tm = require('task-master');
 module.exports = function(grunt) {
   tm(grunt);
   grunt.registerTask('mocha', ['mochaTest:test']);
-  grunt.registerTask('default', ['jshint:all', 'mocha']);
+  grunt.registerTask('default', ['jshint:all', 'mocha', 'testem:ci:browser']);
   grunt.registerTask('coverage', ['mochacov:html']);
-  grunt.registerTask('travis', ['jshint:all', 'mocha', 'mochacov:lcov']);
-}
+  grunt.registerTask('travis', ['jshint:all', 'testem:ci:browser', 'mocha', 'mochacov:lcov']);
+  grunt.registerTask('browser', ['testem:run:browser']);
+};
---
+++ tasks/coffee.js
@@ -0,0 +1,5 @@
+module.exports = {
+  test: {
+    'test/compiled/test.safe.js': ['node_modules/mocha-given/lib/mocha-given.coffee', 'test/lodash.coffee', 'test/underscore.coffee']
+  }
+};
